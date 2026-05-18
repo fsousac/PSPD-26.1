b75e84c48892fa062e1d5fdc90960c58e630d3e1
Felipe Sousa <felipescoelho517@gmail.com>
2026-05-18 13:47:14 -0300

    enchance openmp

--- teste_openmp.c
+++ teste_openmp.c
@@ -2,9 +2,24 @@
 #include <omp.h>
 
 int main() {
+    omp_set_num_threads(10);
+    printf("Imprimindo sequencial\n");
 #pragma omp parallel
     {
-        printf("Hello World!\n");
+        int thid, nthreads;
+        thid = omp_get_thread_num();
+        nthreads = omp_get_num_threads();
+        printf("Hello World from %d/%d!\n", thid, nthreads);
     }
+
+    printf("Imprimindo sequencial de novo\n");
+#pragma omp parallel num_threads(3)
+    {
+        int thid, nthreads;
+        thid = omp_get_thread_num();
+        nthreads = omp_get_num_threads();
+        printf("Hello World from %d/%d!\n", thid, nthreads);
+    }
+    printf("The end!\n");
     return 0;
 }
